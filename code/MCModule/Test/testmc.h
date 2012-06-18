7336e5ac07a160c3a73625be357a73196998b420
wanghui19841109@gmail.com <wanghui19841109@gmail.com@9a6adada-617f-8b0c-c7d7-10c13b6d002b>
2012-06-18 07:25:45 +0000

    implementation in testori.cpp create scalar field open marching cube save marching cube

---
+++ code/MCModule/Test/testmc.h
@@ -0,0 +1,13 @@
+#ifndef TESTMC_H
+#define TESTMC_H
+
+//Testing, Hui Wang, May 30, 2012
+
+#include "../MarchingCube/datadef.h"
+#include "../MarchingCube/CIsoSurface.h"
+#include "../MarchingCube/cnonobtusemc.h"
+#include "../MarchingCube/tilingnonobtusemc.h"
+
+int testmc(int argc, char* argv[]);
+
+#endif // TESTMC_H
