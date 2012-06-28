33ec84d01ccbfef5d75d2d2f90e09337b61fabca
jjcao1231@gmail.com <jjcao1231@gmail.com@9a6adada-617f-8b0c-c7d7-10c13b6d002b>
2012-06-28 09:10:14 +0000

    branch 1 OK for Distance field generation, use CNonobtuseMC to generate non-obtuse triangular mesh. The TilingNonobtuseMC is still failed to generate non-obtuse triangular mesh. The class may contain wrong code or we still do not understand how to use it with some other processing.

---
+++ trunk/code/MCModule_b1/Test/testori.h
@@ -0,0 +1,21 @@
+#ifndef TESTORI_H
+#define TESTORI_H
+
+#include "../MarchingCube/CIsoSurface.h"
+#include "../MarchingCube/cnonobtusemc.h"
+#include "../MarchingCube/tilingnonobtusemc.h"
+#include "../MarchingCube/renderer.h"
+#include "../MarchingCube/nonobtuse.h"
+#include "../MeshParser/smfparser.h"
+#include <iostream>
+#include <sstream>
+#include <stdlib.h>
+#include <string.h>
+#include <GL/glut.h>
+
+using namespace std;
+
+// test original code
+int testori(int argc, char* argv[]);
+
+#endif // TESTORI_H
